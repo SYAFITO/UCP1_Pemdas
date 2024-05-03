49efc8201bd9231fc2f8b8070c10143296ceb036
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-03 17:03:25 +0700

    Add project files.

---
+++ UCP1_Pemograman dasar/UCP1_Pemograman dasar.cpp
@@ -0,0 +1,60 @@
+// 1. int nPeserta,string nim,int nBilangan
+// 2. int main()
+//    
+//    char nama(20)
+//    string asal sekolah
+// 
+//    cout << "Masukkan nama" << endl;
+//    cin >> nama;
+// 
+//    cout << "Masukkan asal sekolah" << endl;
+//    cin >> asal sekolah;
+// 3. i = 1
+//    while i <= 5
+//    print (i)
+//    i += 1
+//    untuk mencetak angka 1 sampai 5 dengan menggunakan while loop
+// 4.  if (nBilangan % 2 ==0){
+//  nBilangan == nBilangan + 1;
+//   }
+
+//    cout << "Nilai akhir = " << nBilangan << endl;
+// 5. struct mata kuliah {
+//    string kode matkul;
+//    string jumlah sks;
+//    string dosen pengampu;
+//   };
+//     Digunakan untuk menyimpan data yang berhubungan dengan mata kuliah
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int nSehat, nTidakSehat, nRerata;
+    string status;
+
+    cout << "Masukkan Nilai Sehat = " << endl;
+    cin >> nSehat;
+
+    cout << "Masukkan Nilai Tidak Sehat = " << endl;
+    cin >> nTidakSehat;
+
+    nRerata = nSehat + nTidakSehat / 2;
+
+
+    if (nRerata > 75) {
+        status = "Sehat";
+
+    }
+    else {
+        status = "Tidak Sehat";
+    }
+    cout << "Tampilkan status = " << status << endl;
+
+    return 0;
+}
+
+
+
+
